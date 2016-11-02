6d19a02eda5d22e819e4561645f563c3600a81de
Joep Schyns <joepschyns@gmail.com>
2016-11-02 20:49:14 +0100

    Conversion to nunjucks and view for activities

--- server.js
+++ server.js
@@ -1,58 +1,54 @@
 var fs = require('fs');
 var express = require('express');
-var exphbs  = require('express-handlebars');
+var nunjucks = require('nunjucks')
 var strava = require('strava-v3');
 var Promise = require('promise');
 var app = express();
-app.engine('handlebars', exphbs({defaultLayout: 'main'}));
-app.set('view engine', 'handlebars');
+nunjucks.configure('views', {
+    autoescape: true,
+    express: app
+});
 var https = require('https').createServer({
   key: fs.readFileSync('/etc/letsencrypt/live/joepschyns.me/privkey.pem'),
   cert: fs.readFileSync('/etc/letsencrypt/live/joepschyns.me/cert.pem')
 }, app);
 app.use(express.static(__dirname + '/public'));
 
-
-
-
 app.get('/', function (req, res) {
    var s = strava.oauth.getRequestAccessURL({scope:"view_private"});
-   res.render('login',{url:decodeURIComponent(s)});
+  res.render(__dirname + '/views/index.html',{url:decodeURIComponent(s)});
 });
 var code;
-app.get('/oauth',function(req,res){ //switch to https://github.com/barc/express-hbs
+app.get('/oauth',function(req,res){
 	code = req.param('code');
 
 	if(req.param('error') == undefined){ //if no errors in oauth
 		strava.oauth.getToken(code,function(err,result){
-
-			res.render('oauthresult',{athlete:result.athlete,helpers : {
-				activities : function(){
-					strava.athlete.listActivities({},function(err,result){
-						var activityPromises = [];
-						for (var i = 0; i < result.length; i++) {
-							var promise = new Promise (function(resolve,reject){
-								strava.activities.get({id:result[i].id},function(err,result){
-								activityPromises.push(result);
-								if(!err) //no errors
-									resolve(result);
-								else
-									reject(err);
-								});
-							});
-							activityPromises.push(promise);
-						}
-						Promise.all(activityPromises).then(function(values) {
-							console.log('succes getting activities');
-							console.log(values[0].name);
-							return "succes";
-						}).catch(function(reason){
-							console.log('error in getting activities');
-							console.log(reason)
-						});					
+			var athlete = result.athlete;
+			strava.athlete.listActivities({},function(err,result){
+				var activityPromises = [];
+				for (var i = 0; i < result.length; i++) {
+					var promise = new Promise (function(resolve,reject){
+						strava.activities.get({id:result[i].id},function(err,result){
+						activityPromises.push(result);
+						if(!err) //no errors
+							resolve(result);
+						else
+							reject(err);
+						});
 					});
+					activityPromises.push(promise);
 				}
-			}});
+				Promise.all(activityPromises).then(function(values) {
+					console.log('succes getting activities');
+					res.render(__dirname + '/views/oauthresult.html',{athlete:athlete,activities:values});
+				}).catch(function(reason){
+					console.log('error in getting activities');
+					console.log(reason)
+				});					
+			});
+				
+			//res.render(__dirname + '/views/oauthresult.html',{athlete:result.athlete});
 		});
 		
 	}else
